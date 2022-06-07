4ca9ea0f7d0659b6c34d0309c10892fcede141b6
Nicholas Easter <nicholas.easter17@gmail.com>
2022-06-07 15:57:20 -0700

    updated fonts; made font-size bigger for nav bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,8 +8,8 @@ export const NavBar = () => {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav align-items">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Home</a>
                         </li>
@@ -19,11 +19,6 @@ export const NavBar = () => {
                         <li class="nav-item">
                             <a class="nav-link" href="#">Purchase our Danglers</a>
                         </li>
-                        {/*
-                        just a reminder that i can disable the links in the navbar
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>*/}
                     </ul>
                 </div>
             </div>
