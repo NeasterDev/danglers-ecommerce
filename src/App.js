7cd58825a82622aa0f4a56893fa6953955c81dbb
Nicholas Easter <nicholas.easter17@gmail.com>
2022-06-06 20:54:41 -0700

    init commit (created basic navbar also)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import bootstrap from 'bootstrap';
+import './App.css';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
