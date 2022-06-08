d16353763fbfdcad7136528ad4d50bb59ae88768
Nicholas Easter <nicholas.easter17@gmail.com>
2022-06-07 19:52:37 -0700

    added hashlink to be able to click about and it takes you to home page and go to anchro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import bootstrap from 'bootstrap';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 import { NavBar } from './components/NavBar';
 import { Home } from './pages/Home';
+import { Store } from './pages/Store';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+      </Routes>
     </div>
   );
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 export const NavBar = () => {
     return (
         <nav className='navbar navbar-expand-lg bg-light'>
             <div className='container-fluid'>
                 <a className='navbar-brand'>Danglers</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav align-items">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav align-items">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About Us</a>
+                        <li className="nav-item">
+                            <HashLink className="nav-link" to="/#about">About Us</HashLink>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Purchase our Danglers</a>
+                        <li className="nav-item">
+                            <Link className='nav-link' to='/store'>Purchase Our Danglers</Link>
                         </li>
                     </ul>
                 </div>
